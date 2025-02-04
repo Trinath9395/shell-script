da9ba1f3928bcee0095f38ca414113ef30ca40ed
Trinath9395 <trinathdevops0606@gmail.com>
2025-02-04 16:59:05 +0530

    datatypes script added

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -3,8 +3,13 @@
 NUMBER1=$1
 NUMBER2=$2
 
+if [ -z "$NUMBER1" ] || [ -z "$NUMBER2" ]; then
+  echo "please give the arguments to the script"
+  exit 1
+fi
+
 TIMESTAMP=$(date)
-echo "script execuated at: $TIMESTAMP"
+echo "script executed at: $TIMESTAMP"
 SUM=$((NUMBER1+NUMBER2))
 echo "sum of $NUMBER1 and $NUMBER2 is $SUM"
 
