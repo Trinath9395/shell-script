90355fdfc2ebfb8eaa1ac632fc224d3fc7e32c24
Trinath9395 <trinathdevops0606@gmail.com>
2025-02-04 15:45:52 +0530

    new variable added

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=Trinath
+PERSON2=mom
+
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: hi $PERSON1"
+echo "$PERSON1: How you doing?"
+echo "$PERSON2: nice what about you?"
+echo "$PERSON1: I am doing good"
