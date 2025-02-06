d40883de183c2b3c12f5c11f028d2cf26a4ca2aa
Trinath9395 <trinathdevops0606@gmail.com>
2025-02-06 17:04:43 +0530

    added simple script

--- 15-diskusage.sh
+++ 15-diskusage.sh
@@ -2,11 +2,19 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESOLD=5
+MSG=""
 
 while read -r line
 do 
   USAGE=$(echo $line | awk -F " " '{print $6}')
   PARTION=$(echo $line | awk -F " " '{print $NF}')
   echo "Partion of: $PARTION, usage $USAGE"
+  if [ $USAGE -ge $DISK_THRESOLD ]; then
+   MSG+="HIGH disk usage on partion on: $PARTION usage is: $USAGE \n
+  fi
+
 done <<< "$DISK_USAGE"
 
+ echo -e "Message: $MSG"
+
+ echo "$MSG" | sendmail -s "High disk usage" thrinathreddy.d@gmail.com
