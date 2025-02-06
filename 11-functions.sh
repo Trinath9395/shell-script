9989de011002460b77a2530073a078593cdfde1f
Trinath9395 <trinathdevops0606@gmail.com>
2025-02-06 11:07:44 +0530

    added simple script

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,6 +2,11 @@
 
 USERID=$(id -u)
 
+LOGS_FOLDER="var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date '+%Y%m%d%H%M%S')
+LOG_FILE_NAME=$LOGS_FOLDER/LOG_FILE-$TIMESTAMP.log
+
 VALIDATE(){
     if [ $1 -ne 0 ]; then
      echo "$2 ...Failure"
---
+++ sample.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ trinath -z ]; then
+ echo "trinath is there"
+else
+ echo "trinath is not there"
+fi
