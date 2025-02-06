fe9dd706e62e7212727d6cfd580c506d67a6a118
Trinath9395 <trinathdevops0606@gmail.com>
2025-02-06 12:41:40 +0530

    added simple script

--- 14-backup.sh
+++ 14-backup.sh
@@ -7,54 +7,48 @@ DEST_DIR=$2
 DAYS=${3:-14} 
 
 LOGS_FOLDER="/home/ec2-user/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d '.' -f1)
-TIMESTAMP=$(date +%Y-m%-d%-H%-M%-S%)
+LOG_FILE=$(basename "$0" | cut -d '.' -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+mkdir -p "$LOGS_FOLDER"
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-     echo "$2...Failure"
+     echo "$2...Failure" | tee -a "$LOG_FILE_NAME"
     else
-     echo "$2...Success"
+     echo "$2...Success" | tee -a "$LOG_FILE_NAME"
     fi
 }
 
-echo "Script exectuion starting time at: $TIMESTAMP" >>"$LOG_FILE_NAME"
+echo "Script execution started at: $TIMESTAMP" | tee -a "$LOG_FILE_NAME"
 
 USAGE(){
-    echo "USAGE:: sh sh 14-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(optinal)>" 
+    echo "USAGE:: sh 14-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(optional)>" | tee -a "$LOG_FILE_NAME"
     exit 1
 }
-
-mkdir -p /home/ec2-user/shellscript-logs
 
 if [ $# -lt 2 ]; then
  USAGE
 fi
 
-if [ ! -d $SOURCE_DIR ]; then
- echo "$SOURCE_DIR does not exist...Please check"
+if [ ! -d "$SOURCE_DIR" ]; then
+ echo "$SOURCE_DIR does not exist...Please check" | tee -a "$LOG_FILE_NAME"
  exit 1
 fi
 
-if [ ! -d $DEST_DIR ]; then
- echo "$DEST_DIR does not exist...Please Check"
+if [ ! -d "$DEST_DIR" ]; then
+ echo "$DEST_DIR does not exist...Please Check" | tee -a "$LOG_FILE_NAME"
  exit 1
 fi
 
-FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
-echo "Files are: $FILES"
+FILES=$(find "$SOURCE_DIR" -name "*.log" -mtime +"$DAYS")
 
-if [[ -n $FILES ]]; then
- echo "FILES are $FILES"
- sudo ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
- $FILES | zip -@ $ZIP_FILE
+if [[ -n "$FILES" ]]; then
+ echo "Files to be zipped: $FILES" | tee -a "$LOG_FILE_NAME"
+ ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+ echo "$FILES" | zip -@ "$ZIP_FILE"
+ VALIDATE $? "Zipping log files"
 else
- echo "no files found to zip $DAYS"
+ echo "No files found to zip older than $DAYS days." | tee -a "$LOG_FILE_NAME"
 fi
-
-
-
-
-
-
