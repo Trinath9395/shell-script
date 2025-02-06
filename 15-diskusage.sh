3ccf42af00c80a6ca797406646cc094549acd7a8
Trinath9395 <trinathdevops0606@gmail.com>
2025-02-06 16:49:34 +0530

    added simple script

--- 15-diskusage.sh
+++ 15-diskusage.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESOLD=5
 
 while read -r line
 do 
- echo "$line"
+  USAGE=$(echo "$line" | awk -F " " '{print $6}')
+
 done <<< "$DISK_USAGE"
 
