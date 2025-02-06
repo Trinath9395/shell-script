4cfb33865925e37c3bacc01d4d257160975f067d
Trinath9395 <trinathdevops0606@gmail.com>
2025-02-06 16:54:34 +0530

    added simple script

--- 15-diskusage.sh
+++ 15-diskusage.sh
@@ -6,6 +6,7 @@ DISK_THRESOLD=5
 while read -r line
 do 
   USAGE=$(echo $line | awk -F " " '{print $6}')
-
+  PARTION=$(echo $line | awk -F " " '{print $NF}')
+  echo "Partion of: $PARTION, usage $USAGE"
 done <<< "$DISK_USAGE"
 
