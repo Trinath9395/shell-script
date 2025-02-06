c3cf10c53674113c3b5a4478b4d98fe6e57c76fb
Trinath9395 <trinathdevops0606@gmail.com>
2025-02-06 12:10:07 +0530

    added simple script

--- 14-backup.sh
+++ 14-backup.sh
@@ -42,7 +42,7 @@ if [ ! -d $DEST_DIR ]; then
  exit 1
 fi
 
-FILES=$(find $SOURCE_DIR -name "*.log" +mtime $DAYS)
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 echo "Files are: $FILES"
 
 
